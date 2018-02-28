8a3c5615301ea398391199ad3db9d4172f801656
Mark Mortensen <mrostgaard@gmail.com>
2018-02-28 15:25:20 +0100

    Updated test cases

--- 040-streams/Main.scala
+++ 040-streams/Main.scala
@@ -8,28 +8,42 @@ import fpinscala.laziness.Stream._
 
 // this is how we do simple interactive testing
 
-val l1 :Stream[Int] = Empty
-val l2 :Stream[Int] = empty
+//val l1 :Stream[Int] = Empty
+//val l2 :Stream[Int] = empty
 
-val l3 :Stream[Int]= cons(1, cons(2, cons (3, empty)))
+//val l3 :Stream[Int]= cons(1, cons(2, cons (3, empty)))
 
-println (l1.headOption)
-println (l2.headOption)
-println (l3.headOption)
+//println (l1.headOption)
+//println (l2.headOption)
+//println (l3.headOption)
 
 
 val naturals = from(0)
-println (naturals.take(500).toList)
+//println (naturals.take(500).toList)
 
-val to10 = to(10).toList
-val to20 = to(20).toList
+//val to10 = to(10).toList
+//val to20 = to(20).toList
 
-println(to10)
-println(to20)
+//println(to10)
+//println(to20)
 
-val naturals2 = naturals.takeWhile(_<1000000000).drop(100).take(50).toList
-println(naturals2)
+//val naturals2 = naturals.takeWhile(_<1000000000).drop(100).take(50).toList
+//println(naturals2)
 //Because laizily evaluation will just drop the first 100 and then take the next 50 (101 -> 150) and turn that into the list
 
-assert(naturals.forAll(_ < 0))
-assert(naturals.forAll(_ >= 0))
+//assert(naturals.forAll(_ < 0))
+//assert(naturals.forAll(_ >= 0))
+
+//Exercise 6
+//val naturals2 = naturals.takeWhileFold(_<1000000000).drop(100).take(50).toList
+//print(naturals2)
+
+//Exercise 7
+val headOptionNone = naturals.take(0).headOption()
+assert(headOptionNone == None)
+val headOption1 = naturals.take(10).headOption()
+assert(headOption1 == Some(0))
+val headOption2 = naturals.drop(5).take(10).headOption()
+assert(headOption1 == Some(6))
+
+//Exercise 8
