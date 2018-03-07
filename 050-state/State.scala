3fa9f20cc6f64095ba8c581e1f65302e99fde292
matho <markus.08xmt@gmail.com>
2018-03-07 13:09:11 +0100

    Add upt to ex 9

--- 050-state/State.scala
+++ 050-state/State.scala
@@ -16,24 +16,48 @@ object RNG {
 
   // Exercise 1 (CB 6.1)
 
-  // def nonNegativeInt(rng: RNG): (Int, RNG) = { ...
+  def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
+      //I make a new function call if this happens, because having it point to a constant int, would make that int twice as likely to be randomly generated.
+      case (i,s) if(i == Int.MinValue) => nonNegativeInt(rng) 
+      case (i,s) if(i < 0) => (i * -1, s)
+      case (i,s) => (i,s)
+  }
+  
 
   // Exercise 2 (CB 6.2)
 
-  // def double(rng: RNG): (Double, RNG) = { ...
-
+  def double(rng: RNG): (Double, RNG) = { 
+    val t = this.nonNegativeInt(rng)
+    val d = t._1.toDouble / Int.MaxValue.toDouble
+    (d,t._2)
+  }
   // Exercise 3 (CB 6.3)
 
-  // def intDouble(rng: RNG): ((Int, Double), RNG) = { ...
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val it = nonNegativeInt(rng)
+    val dt = double(rng)
+    ((it._1,dt._1),it._2)
+  }
 
-  // def doubleInt(rng: RNG): ((Double, Int), RNG) = { ...
-
-  // def boolean(rng: RNG): (Boolean, RNG) =
-  //  rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val it = nonNegativeInt(rng)
+    val dt = double(rng)
+    ((dt._1,it._1),it._2)
+  }
+  def boolean(rng: RNG): (Boolean, RNG) =
+    rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
 
   // Exercise 4 (CB 6.4)
 
-  // def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if(count < 0)
+      (List[Int](),rng)
+    else {
+      val t1 = nonNegativeInt(rng)
+      val t2 = ints(count-1)(t1._2)
+      (t1._1 :: t2._1, t2._2)
+    }
+  }
 
   // There is something terribly repetitive about passing the RNG along
   // every time. What could we do to eliminate some of this duplication
@@ -52,34 +76,54 @@ object RNG {
       (f(a), rng2)
     }
 
-  // def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
   // Exercise 5 (CB 6.5)
 
-  // val _double: Rand[Double] =
+  val _double2: Rand[Double] = map(nonNegativeInt)(i => i.toDouble / Int.MaxValue)
 
   // Exercise 6 (CB 6.6)
 
-  // def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rnga) = ra(rng)
+      val (b, rngb) = rb(rnga)
+      (f(a,b),rngb)
+    }
 
   // this is given in the book
 
-  // def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
-  //  map2(ra, rb)((_, _))
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
 
-  // val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
 
-  // val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
   // Exercise 7 (6.7)
 
-  // def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = fs match {
+    case x :: xs => {
+        rng =>{
+          val (tail,rng2) = sequence(xs)(rng)
+          (x(rng)._1 :: tail, rng2)
+        }
+      }
+    case Nil => {
+        rng => (List[A](), rng)
+      }
+    }
 
-  // def _ints(count: Int): Rand[List[Int]] = ...
+  def _ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(map(nonNegativeInt)(i => i)))
 
   // Exercise 8 (6.8)
 
-  // def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ...
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng =>{
+      val(a,rnga) = f(rng)
+      g(a)(rnga)
+    }
 
   // def nonNegativeLessThan(n: Int): Rand[Int] = { ...
 
@@ -91,7 +135,13 @@ case class State[S, +A](run: S => (A, S)) {
 
   // Exercise 9 (6.10)
 
-  // def map[B](f: A => B): State[S, B] = ...
+  def map[B](f: A => B): State[S, B] = {
+    def run2 = s:S =>{
+      val (a, s2) = run(s)
+      (f(a),s2)
+    }
+    State[A, B](run2)
+  }
 
   // def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ...
 
