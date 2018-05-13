d26ec9cbd9f30a107f7b381511cb0a75c1830be7
matho <markus.08xmt@gmail.com>
2018-05-13 23:41:10 +0200

    Add F1 score generation

--- 130-sentiment-analysis/src/main/scala/Main.scala
+++ 130-sentiment-analysis/src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import scala.collection.mutable.WrappedArray
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions.explode
@@ -14,6 +14,9 @@ import org.apache.spark.sql.functions.collect_set
 import org.apache.spark.sql.functions._;
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
 
 
 
@@ -86,12 +89,14 @@ object Main {
     
     val trainingData = labels.select("id","label").join(reviewVectors, "id")
 
+    trainingData.show
+
     val splits = trainingData.randomSplit(Array(0.9, 0.1), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
     
     
-    val layers = Array[Int](50, 5, 4, 3)
+    val layers = Array[Int](50, 30, 10, 3)
 
     val trainer = new MultilayerPerceptronClassifier()
       .setLayers(layers)
@@ -103,10 +108,14 @@ object Main {
 
     val result = model.transform(test)
     val predictionAndLabels = result.select("prediction", "label")
+
+    val scoreAndLabels = predictionAndLabels.map(row => (row.getAs[Double](0),row.getAs[Int](1).toDouble)).rdd
     val evaluator = new MulticlassClassificationEvaluator()
       .setMetricName("accuracy")
-
+    val metrics = new MulticlassMetrics(scoreAndLabels)
     println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+    println(s"Weighted F1 score: ${metrics.weightedFMeasure}")
+    
     spark.stop
   }
 
